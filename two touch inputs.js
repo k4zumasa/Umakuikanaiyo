889846def57628772a1a43a7b4c550d2e187c1ce
k4zumasa <31819545+k4zumasa@users.noreply.github.com>
2021-06-18 01:21:12 +0900

    make cursor draggable

--- two touch inputs.js
+++ two touch inputs.js
@@ -24,37 +24,41 @@ function create ()
     //  We need 3 extra pointers, as we only get 1 by default
     this.input.addPointer(3);
 
-//    text = this.add.text(10, 10, 'Use up to 4 fingers at once', { font: '16px Courier', fill: '#00ff00' });
+    //オブジェクトとしてカーソルを表示
+    var cursor = this.add.rectangle(200, 150, 148, 148, 0x0a0a0a).setInteractive();
+
+    //カーソルをdraggableに
+    this.input.setDraggable(cursor);
+    this.input.on('dragstart', function(pointer, dragX, dragY){
+        // ...
+    })
+    this.input.on('drag', function(pointer, dragX, dragY){
+        gameObject.setPosition(dragX, dragY);
+    })
+    this.input.on('dragend', function(pointer, dragX, dragY, dropped){
+        // ...
+    })
 }
 
 //毎フレーム実行される
 function update ()
 {
-    if (this.input.pointer1.isDown || this.input.pointer2.isDown || this.input.pointer3.isDown || this.input.pointer4.isDown)
-    {
-        graphics.clear();
-    }
+//    if (this.input.pointer1.isDown || this.input.pointer2.isDown || this.input.pointer3.isDown || this.input.pointer4.isDown)
+//    {
+//        graphics.clear();
+//    }
 
-//    text.setText([
-//        'pointer1.isDown: ' + this.input.pointer1.isDown,
-//    ]); //
+//    graphics.fillRect(
+//        window.innerWidth/2 - rectWidth/2,
+//        this.input.pointer1.y,
+//        rectWidth,
+//        rectHeight);
 
-    //操作できるカーソルを描画
-    graphics.fillStyle(0x0a0a0a, 1);
-    graphics.fillRect(
-        window.innerWidth/2 - rectWidth/2,
-        this.input.pointer1.y,
-        rectWidth,
-        rectHeight);
-
-    //縞模様を描画
     for (let i = 0; i < 6; i++) {
         graphics.fillRect(window.innerWidth/2 - rectWidth/2,
         window.innerHeight/2 - rectHeight/2*(-11 + i*4),
         rectWidth,
         rectHeight);
     }
-
-
 
 }
