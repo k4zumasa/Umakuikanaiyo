516acd9e8afd05126e32fb374061ed2f651c0697
Nira-Yu <81912163+Nira-Yu@users.noreply.github.com>
2021-06-17 20:14:29 +0900

    Update two touch inputs.js

--- two touch inputs.js
+++ two touch inputs.js
@@ -15,6 +15,9 @@ var text;
 var rectWidth = 1000; //200
 var rectHeight = 130; //60
 var iconWidth = 600;
+var pre_y = 0;
+var new_y = 0;
+var dy = 0;
 
 var game = new Phaser.Game(config);
 
@@ -39,20 +42,22 @@ function update ()
 //    text.setText([
 //        'pointer1.isDown: ' + this.input.pointer1.isDown,
 //    ]); //
+    new_y = this.input.pointer1.y;
+    dy = this.input.pointer1.y - pre_y;
+    graphics.top += dy
+    document.write(graphics.top)
 
     //操作できるカーソルを描画
     graphics.fillStyle(0xffffff, 1);//graphics.fillStyle(0x0a0a0a, 1)
     graphics.fillRect(window.innerWidth/2 - iconWidth/2, this.input.pointer1.y-60, iconWidth, rectHeight*2);
     //graphics.fillRect(window.innerWidth/2 - rectWidth/2, this.input.pointer1.y,200, rectHeight*2);
-    document.write(this.input.pointer1.y-60);//座標
+    //document.write(this.input.pointer1.y-60);//座標
+    pre_y =this.input.pointer1.y
 
     //縞模様を描画
     for (let i = 0; i < 20; i++) {
         graphics.fillRect(-20, i*2*rectHeight ,rectWidth, rectHeight);
         //graphics.fillRect(window.innerWidth/2 - rectWidth/2,window.innerHeight/2 - rectHeight/2*(-11 + i*4),rectWidth,rectHeight);
     }
-    
-
-
-
+   
 }
