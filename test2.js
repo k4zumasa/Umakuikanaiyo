d1bdebda22e87afd34f680976d8849ad1f3f7007
k4zumasa <31819545+k4zumasa@users.noreply.github.com>
2021-06-23 14:51:23 +0900

    衝突処理、音実装

---
+++ test2.js
@@ -0,0 +1,125 @@
+function preload ()
+{
+    this.load.audio('suiteki', ['assets/suiteki.mp3']);
+    this.load.audio('suityu', ['assets/suityu.mp3']);
+
+    this.load.image('thumb', 'assets/thumb.png');
+
+}
+
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: '#badae8',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    audio: {
+        disableWebAudio: true
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var cursor;
+var cursor2;
+var stripes;
+var graphics;
+var rectangles;
+var cursorWidth = window.innerWidth/1.5;
+var stripeWidth = window.innerWidth/1.5;
+var cursorHeight = 100;
+var stripeHeight = 110;
+var cursorColor = 0xffffff; //0xffffff
+var stripeColor = 0xffffff;
+var suitekiplayed;
+
+function create (){
+
+    this.suityu = this.sound.add('suityu', false);
+    this.suiteki = this.sound.add('suiteki', false);
+
+    //graphicsオブジェクトの生成
+    graphics = this.add.graphics(
+    {fillStyle: { color: cursorColor }
+    });
+
+    rectangles = [];
+
+    //ストライプ生成
+    for(var i = 0; i < 6; i++)
+    {
+        rectangles.push(new Phaser.Geom.Rectangle(
+        window.innerWidth/2 - stripeWidth/2,
+        window.innerHeight/2 - stripeHeight/2*(-11 + i*4),
+        stripeWidth,
+        stripeHeight,
+        cursorColor));
+    }
+
+    //カーソルの生成とドラッグ有効化
+    cursor = new Phaser.Geom.Rectangle(
+        window.innerWidth/2 - cursorWidth/2,
+        0,
+        cursorWidth,
+        cursorHeight, Phaser.Geom.Rectangle.Contains);
+
+    cursor2 = new Phaser.Geom.Rectangle(
+        window.innerWidth/2 - cursorWidth/2,
+        0,
+        cursorWidth,
+        75,
+        Phaser.Geom.Rectangle.Contains);
+
+    this.input.on('pointermove', function (pointer) {
+        Phaser.Geom.Rectangle.CenterOn(cursor, window.innerWidth/2, pointer.y);
+        Phaser.Geom.Rectangle.CenterOn(cursor2, window.innerWidth/2, pointer.y);
+    });
+
+}
+
+
+function update (){
+    graphics.clear();
+    graphics.fillRectShape(cursor);
+
+    for(var i = 0; i < rectangles.length; i++){
+        graphics.fillRectShape(rectangles[i]);
+    }
+
+    if (Phaser.Geom.Intersects.RectangleToRectangle(cursor2, rectangles[0])
+        || Phaser.Geom.Intersects.RectangleToRectangle(cursor2, rectangles[1])
+        || Phaser.Geom.Intersects.RectangleToRectangle(cursor2, rectangles[2])
+        || Phaser.Geom.Intersects.RectangleToRectangle(cursor2, rectangles[3])
+        || Phaser.Geom.Intersects.RectangleToRectangle(cursor2, rectangles[4])
+        || Phaser.Geom.Intersects.RectangleToRectangle(cursor2, rectangles[5]) ){
+
+//         console.log("intersected");
+           if(this.suiteki.isPlaying){
+               this.suiteki.stop();
+           }
+           if(!this.suityu.isPlaying){
+               this.suityu.play();
+           }
+           suitekiplayed = false;
+
+    }else{
+//       console.log("not intersected");
+//        this.suityu.stop();
+//        this.suiteki.play();
+//
+       if(this.suityu.isPlaying){
+        this.suityu.stop();
+       }
+//
+       if(!this.suiteki.isPlaying
+          && !suitekiplayed){
+           this.suiteki.play();
+           suitekiplayed = true
+        }
+    }
+}
--- two touch inputs.js
+++ two touch inputs.js
@@ -3,7 +3,7 @@ var config = {
     parent: 'phaser-example',
     width: window.innerWidth,
     height: window.innerHeight,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#badae8',
     scene: {
         create: create,
         update: update
@@ -15,8 +15,8 @@ var text;
 var rectWidth = window.innerWidth/1.5 ;
 var rectHeight = 100;
 var cursorHeight = 100;
-var cursorColor = 0x000000; //0xffffff
-var stripeColor = 0x000000;
+var cursorColor = 0xffffff; //0xffffff
+var stripeColor = 0xffffff;
 
 var game = new Phaser.Game(config);
 
@@ -25,33 +25,29 @@ function create ()
     graphics = this.add.graphics();
 
     //オブジェクトとしてカーソルを表示
+//    var rect = new Phaser.Geom.Rectangle(100, 100, 30, 30);
     var cursor = this.add.rectangle(window.innerWidth/2, 0, rectWidth, cursorHeight, cursorColor).setInteractive();
 
     //カーソルをdraggableに
     this.input.setDraggable(cursor);
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        gameObject.y = dragY;
+    this.input.on('drag', function (pointer, cursor, dragX, dragY) {
+        cursor.y = dragY;
     });
 
-
     for (let i = 0; i < 6; i++) {
-        graphics.fillStyle(stripeColor, 1);
+//        graphics.fillStyle(stripeColor, 1);
         graphics.fillRect();
         var stripe = this.add.rectangle(window.innerWidth/2,
                                           window.innerHeight/2 - rectHeight/2*(-11 + i*4),
                                           rectWidth,
                                           rectHeight, cursorColor);
-
     }
+
 }
 
 //毎フレーム実行される
-function update ()
-{
+function update (){
+    cursor.getTopCenter();
 
-//    if (this.input.pointer1.isDown || this.input.pointer2.isDown || this.input.pointer3.isDown || this.input.pointer4.isDown)
-//    {
-//        graphics.clear();
-//    }
 
 }
